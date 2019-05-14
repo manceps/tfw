11973847559582d34b3622713215cf56b60f13dc
hobs <hobs+github@totalgood.com>
2019-05-14 14:59:39 -0700

    add pyscaffolding for pip installable package and redundant .gitignores

---
+++ README.rst
@@ -0,0 +1,19 @@
+===
+tfw
+===
+
+
+TensorFlow World demos on natural language distance metrics, Mueller Report unredaction, and adversarial attack defense.
+
+
+Description
+===========
+
+A longer description of your project goes here...
+
+
+Note
+====
+
+This project has been set up using PyScaffold 3.1. For details and usage
+information on PyScaffold see https://pyscaffold.org/.
--- src/tfw/wsgi.py
+++ src/tfw/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nlpia_bot project.
+WSGI config for tfw project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nlpia_bot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tfw.settings')
 
 application = get_wsgi_application()
