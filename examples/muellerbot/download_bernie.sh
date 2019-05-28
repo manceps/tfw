7006f5cd374cfec70631d1954d82ecae567f9cd9
hobs <hobs+github@totalgood.com>
2019-05-28 12:04:46 -0700

    cleanup bernie

--- examples/muellerbot/download_bernie.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# cd models
-wget -c http://gpu.totalgood.org/midata/models/robot-bernie/bern.iter999.h5 models/bern.iter999.h5
-wget -c http://gpu.totalgood.org/midata/models/robot-bernie/wiki.iter999.h5 models/wiki.iter999.h5
